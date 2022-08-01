7e1f14563c9e751debfc245c45add23f91da8eb1
Gabriel Albino <enggabrielalbino@gmail.com>
2022-07-31 22:56:24 -0300

    Revertendo lista de nós percorridos para encontrar o caminho percorrido, adicionando variavel para controle de distância.

--- grafos/bellman_ford.pl
+++ grafos/bellman_ford.pl
@@ -7,13 +7,21 @@ edge(d,e,2).
 edge(e,b,2).
 
 % Se tem uma aresta ligando a Origem ao Destino, defina que um CaminhoPercorrido = O nó de destino + os nós visitados
-% (Visitados = Os nós percorridos pelo backtracking até então) .
-path(Origem,Destino,Visitados,[Destino|Visitados]):-edge(Origem,Destino,_).
-
+% (Visitados = Os nós percorridos pelo backtracking até então); em uma distância percorrida de 1.
+path(Origem,Destino,Visitados,[Destino|Visitados],1):-edge(Origem,Destino,_).
 % Se existe uma aresta ligando a Origem a um ponto internmediário X; e X != Destino (reduz o backtracking, uma vez que essa é a condição base);
-% e X não foi visitado ainda; e existe uma aresta ligando X ao Destino (Recursão). 
-path(Origem,Destino,Visitados,CaminhoPercorrido):-edge(Origem,X,_), X\=Destino, not(member(X,Visitados)),path(X,Destino,[X|Visitados], CaminhoPercorrido). 
+% e X não foi visitado ainda; e existe uma aresta ligando X ao Destino (Recursão); percorrando o CaminhoPercorrido de nós em uma distância total Distancia. 
+path(Origem,Destino,Visitados,CaminhoPercorrido,Distancia):-edge(Origem,X,_), 
+  X\=Destino, 
+  not(member(X,Visitados)),
+  path(X,Destino,[X|Visitados], CaminhoPercorrido, NovaDistancia),
+  Distancia is NovaDistancia+1.
 
+% Reverte o caminho para que o CaminhoPercorrido seja retornado corretamente
+reverseList([], A, A). 
+reverseList([H|T], A, L2):-reverseList(T, [H|A], L2).
+
+% Clausula NOT para limitar o backtrack apenas nos nós que NÃO foram visitados ainda
 not(P):-P,!,fail;true.
 
 % Adiciona L1 a L2, Setando L3
