9f2148ffdaaad03e1a1cb127fd2630ebdd02210d
sofiapatrocinio <sofiapatrocinio@hotmail.com>
2022-08-01 20:52:56 -0300

    Add menu of list

--- main.pl
+++ main.pl
@@ -3,7 +3,7 @@ loadModules:- [grafos/big_graph, grafos/shortest_path].
 menu:- loadModules, repeat,
 	write('=== MENU ==='), nl,
 	write('1. Grafos'), nl,
-	write('2. Option B'), nl,
+	write('2. Lista'), nl,
 	write('0. Exit'), nl,
 	read(X),
 	option(X),
@@ -12,7 +12,7 @@ menu:- loadModules, repeat,
 
 option(0):- !.
 option(1):- menuGrafos, !.
-option(2):- write('You choose the option B...'), nl, !.
+option(2):- menuLista, !.
 option(_):- write('It is not an acceptable option'), nl, !.
 
 menuGrafos:- repeat,
@@ -31,4 +31,23 @@ optionGrafos(2) :- time(path(18,65,[18],CaminhoPercorrido,_)), write(CaminhoPerc
 optionGrafos(2) :- repeat, time(path(18,65,[18],CaminhoPercorrido2,_)),reverseList(CaminhoPercorrido2, [], CaminhoPercorrido), write(CaminhoPercorrido), nl, !.
 optionGrafos(_) :- write('Opção inválida'), nl, !.
 
+menuLista:- repeat,
+	write('=== LISTA ==='), nl,
+	write('1. Criar uma lista com 1000 elementos'), nl,
+	write('2. Adiciona um valor a lista'), nl,
+	write('3. Remove último elemento da lista'), nl,
+	write('4. Implementa reverse na lista'), nl,
+	write('5. Pegar maior valor da lista'), nl,
+	write('0. Exit'), nl,
+	read(X),
+	optionLista(X),
+	X==0,
+	!.
 
+optionLista(0) :- !.
+optionLista(1) :- !.
+optionLista(2) :- !.
+optionLista(3) :- !.
+optionLista(4) :- !.
+optionLista(5) :- !.
+optionLista(_) :- write('Opção inválida'), nl, !.
