13a1c3fffcc21498872ff01fb3f9dea91c88f2cb
Wellington Jonathan <wellpriz@gmail.com>
2022-08-01 21:01:44 -0300

    altera Lista2

--- algoritmosdeordenacao/bubblesort.pl
+++ algoritmosdeordenacao/bubblesort.pl
@@ -1,12 +1,8 @@
 
 bubblesort(Lista,OrdLista):-
-    trocar(Lista,Lista2),!, bubblesort(lista2,OrdLista).
+    trocar(Lista,Lista2),!, bubblesort(Lista2,OrdLista).
 maior(X,Y):- X > Y.
 trocar([X,Y|Tail],[X,Y|Tail]):- maior(X,Y).
 
 trocar([Z|Tail],[Z|Tail1]):- trocar(Tail,Tail1).
-
-
-
-
 
